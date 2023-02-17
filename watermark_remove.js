dd71495fdd43d1614602d504529640d45915d8b8
jzf21 <jozefneelamkavil2003@gmail.com>
2023-02-17 19:43:13 +0530

    stuff

--- watermark_remove.js
+++ watermark_remove.js
@@ -1,11 +1,8 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    console.log("I am ready");
-
-    setInterval(()=>{
-        if( document.getElementsByClassName("w-webflow-badge").length!=0)
-        {
-        document.getElementsByClassName("w-webflow-badge")[0].remove();
-        console.log("Gone Boom!!!!");
-        }else{}
-    },0.1);
-  })
+document.addEventListener("DOMContentLoaded", (event) => {
+  setInterval(() => {
+    if (document.getElementsByClassName("w-webflow-badge").length != 0) {
+      document.getElementsByClassName("w-webflow-badge")[0].remove();
+    } else {
+    }
+  }, 0.1);
+});
