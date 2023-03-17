5040f005ee21421d39ef878b9ca0ab1def6d4e0b
jzf21 <jozefneelamkavil2003@gmail.com>
2023-03-17 22:57:20 +0530

    new

--- index.js
+++ index.js
@@ -105736,13 +105736,30 @@ Webflow.require("commerce") &&
     apiUrl: "https://render.webflow.com",
   });
 
-$(".button-card").click(function () {
-  var buttonId = $(this).attr("id");
-  $("#modal-container").removeAttr("class").addClass(buttonId);
-  $("body").addClass("modal-active");
-});
+// $(".button-card").click(function () {
+//   var buttonId = $(this).attr("id");
+//   $("#modal-container").removeAttr("class").addClass(buttonId);
+//   $("body").addClass("modal-active");
+// });
 
-$("#modal-container").click(function () {
-  $(this).addClass("out");
-  $("body").removeClass("modal-active");
-});
+// $("#modal-container").click(function () {
+//   $(this).addClass("out");
+//   $("body").removeClass("modal-active");
+// });
+// const aboutBtn = document.querySelector('.about-btn');
+// const closeBtn = document.querySelector('.close-btn');
+
+// const overlay = document.querySelector('.overlay');
+// const card = document.querySelector('.card-speaker');
+
+
+// aboutBtn.addEventListener('click', ()=>{
+
+//     card.style.cssText = 'top:50%; opacity:1; transform: translateY(-50%)' 
+//     overlay.style.background = 'rgba(0,0,0,0.9)';
+// });
+
+// closeBtn.addEventListener('click', ()=>{
+//     card.style.cssText='top:-200%; opacity:0';
+//     overlay.style.background = 'rgba(0,0,0,0.4)';
+//     })
